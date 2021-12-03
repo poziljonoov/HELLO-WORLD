943165510641e16ba7833129c249727d982f1d30
user.name <kathan@mail_server.com>
2021-12-03 09:59:15 +0200

    HELLO-WORLD

--- HELLO-WORLD/Form1.cs
+++ HELLO-WORLD/Form1.cs
@@ -16,5 +16,10 @@ namespace HELLO_WORLD
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          lebel1.text "HELLO-WORLD";
+        }
     }
 }
